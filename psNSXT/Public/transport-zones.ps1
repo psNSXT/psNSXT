a2e8ce10ad1975fdde9e6cd9a305d51ef29cad90
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-03-11 21:10:48 +0100

    TZ: Add NSXT-TransportZones for add VLAN or Overlay TZ

--- psNSXT/Public/transport-zones.ps1
+++ psNSXT/Public/transport-zones.ps1
@@ -4,6 +4,71 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function Add-NSXTTransportZones {
+
+    <#
+        .SYNOPSIS
+        Add a Transport Zones
+
+        .DESCRIPTION
+        Add a Transport Zones (Vlan or Overlay)
+
+        .EXAMPLE
+        Add-NSXTTransportZones -transport_type VLAN -host_switch_name NVDS-psNSXT -display_name MyTZ-Vlan -description "Add via psNSXT"
+
+        Add a TransportZones type Vlan with NVDS (host_switch_name) NVDS-psNSXT named myTZ-Vlan with an description
+
+        .EXAMPLE
+        Add-NSXTTransportZones -transport_type OVERLAY -host_switch_name NVDS-psNSXT -display_name MyTZ-Overlay -description "Add via psNSXT"
+
+        Add a TransportZones type Overlay with NVDS (host_switch_name) NVDS-psNSXT named myTZ-Overlay with an description
+
+    #>
+
+    Param(
+        [Parameter(Mandatory = $false)]
+        [string]$display_name,
+        [Parameter(Mandatory = $true)]
+        [string]$host_switch_name,
+        [Parameter(Mandatory = $false)]
+        [string]$description,
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("VLAN", "OVERLAY", IgnoreCase = $false)]
+        [string]$transport_type,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultNSXTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = 'api/v1/transport-zones'
+
+        $_tz = new-Object -TypeName PSObject
+
+        $_tz | add-member -name "host_switch_name" -membertype NoteProperty -Value $host_switch_name
+
+        $_tz | add-member -name "transport_type" -membertype NoteProperty -Value $transport_type
+
+        if ( $PsBoundParameters.ContainsKey('display_name') ) {
+            $_tz | add-member -name "display_name" -membertype NoteProperty -Value $display_name
+        }
+
+        if ( $PsBoundParameters.ContainsKey('description') ) {
+            $_tz | add-member -name "description" -membertype NoteProperty -Value $description
+        }
+
+        $response = Invoke-NSXTRestMethod -uri $uri -method 'POST' -body $_tz -connection $connection
+        $response
+
+    }
+
+    End {
+    }
+}
+
 function Get-NSXTTransportZones {
 
     <#
