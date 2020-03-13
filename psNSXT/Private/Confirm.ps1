a614b6f26200fb4e7d3a7d66118a24daf6a42baf
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-03-13 22:17:13 +0100

    Confirm: Add Confrm-NSXTTransportZones (it will be reuse for Remove and Set-NSXTZ)

--- psNSXT/Private/Confirm.ps1
+++ psNSXT/Private/Confirm.ps1
@@ -31,3 +31,43 @@ Function Confirm-NSXTFabricVirtualMachines {
     $true
 
 }
+
+Function Confirm-NSXTTransportZones {
+
+    Param (
+        [Parameter (Mandatory = $true)]
+        [object]$argument
+    )
+
+    #Check if it looks like an Transport Zones element
+
+    if ( -not ( $argument | get-member -name transport_type -Membertype Properties)) {
+        throw "Element specified does not contain a transport_type property."
+    }
+    if ( -not ( $argument | get-member -name host_switch_name -Membertype Properties)) {
+        throw "Element specified does not contain an host_switch_name property."
+    }
+    if ( -not ( $argument | get-member -name host_switch_id -Membertype Properties)) {
+        throw "Element specified does not contain a host_switch_id property."
+    }
+    if ( -not ( $argument | get-member -name host_switch_mode -Membertype Properties)) {
+        throw "Element specified does not contain a host_switch_mode property."
+    }
+    if ( -not ( $argument | get-member -name nested_nsx -Membertype Properties)) {
+        throw "Element specified does not contain a nested_nsx property."
+    }
+    if ( -not ( $argument | get-member -name is_default -Membertype Properties)) {
+        throw "Element specified does not contain a is_default property."
+    }
+    if ( -not ( $argument | get-member -name resource_type -Membertype Properties)) {
+        throw "Element specified does not contain a resource_type property."
+    }
+    if ( -not ( $argument | get-member -name id -Membertype Properties)) {
+        throw "Element specified does not contain a id property."
+    }
+    if ( -not ( $argument | get-member -name display_name -Membertype Properties)) {
+        throw "Element specified does not contain a display_name property."
+    }
+    $true
+
+}
