c9762a289c823d55489eb53dd8529f70f4ba3c2e
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-03-13 22:14:45 +0100

    TZ: Add -host_switch_mode, -nested_nsx -is_default parameters to Add-NSXTTranportZones
    for select Standard or Enhanced mode for switch mode, using with nested NSX and set the default TZ

--- psNSXT/Public/transport-zones.ps1
+++ psNSXT/Public/transport-zones.ps1
@@ -23,6 +23,15 @@ function Add-NSXTTransportZones {
 
         Add a TransportZones type Overlay with NVDS (host_switch_name) NVDS-psNSXT named myTZ-Overlay with an description
 
+        .EXAMPLE
+        Add-NSXTTransportZones -transport_type OVERLAY -host_switch_name NVDS-psNSXT -host_switch_mode ENS
+
+        Add a TransportZones type Overlay with NVDS (host_switch_name) NVDS-psNSXT using Enhanced Datapath
+
+        .EXAMPLE
+        Add-NSXTTransportZones -transport_type OVERLAY -host_switch_name NVDS-psNSXT -nested_nsx -is_default
+
+        Add a TransportZones type Overlay with NVDS (host_switch_name) NVDS-psNSXT and enable NSX nested and default Transport Zone
     #>
 
     Param(
@@ -35,6 +44,13 @@ function Add-NSXTTransportZones {
         [Parameter(Mandatory = $true)]
         [ValidateSet("VLAN", "OVERLAY", IgnoreCase = $false)]
         [string]$transport_type,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("ENS", "STANDARD", IgnoreCase = $false)]
+        [string]$host_switch_mode,
+        [Parameter(Mandatory = $false)]
+        [switch]$nested_nsx,
+        [Parameter(Mandatory = $false)]
+        [switch]$is_default,
         [Parameter(Mandatory = $false)]
         [psobject]$connection = $DefaultNSXTConnection
     )
@@ -58,6 +74,28 @@ function Add-NSXTTransportZones {
 
         if ( $PsBoundParameters.ContainsKey('description') ) {
             $_tz | add-member -name "description" -membertype NoteProperty -Value $description
+        }
+
+        if ( $PsBoundParameters.ContainsKey('host_switch_mode') ) {
+            $_tz | add-member -name "host_switch_mode" -membertype NoteProperty -Value $host_switch_mode
+        }
+
+        if ( $PsBoundParameters.ContainsKey('nested_nsx') ) {
+            if ( $nested_nsx ) {
+                $_tz | Add-Member -name "nested_nsx" -membertype NoteProperty -Value $True
+            }
+            else {
+                $_tz | Add-Member -name "nested_nsx" -membertype NoteProperty -Value $false
+            }
+        }
+
+        if ( $PsBoundParameters.ContainsKey('is_default') ) {
+            if ( $is_default ) {
+                $_tz | Add-Member -name "is_default" -membertype NoteProperty -Value $True
+            }
+            else {
+                $_tz | Add-Member -name "is_default" -membertype NoteProperty -Value $false
+            }
         }
 
         $response = Invoke-NSXTRestMethod -uri $uri -method 'POST' -body $_tz -connection $connection
