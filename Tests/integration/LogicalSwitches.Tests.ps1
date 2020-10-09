9869f4ea8b25cda7c472c933ca523de5056208c7
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-10-09 12:24:40 +0200

    LogicalSwitches(Tests): Add test with search by Transport Zone (Id)

--- Tests/integration/LogicalSwitches.Tests.ps1
+++ Tests/integration/LogicalSwitches.Tests.ps1
@@ -9,6 +9,7 @@ Describe "Get Logical Switches" {
     BeforeAll {
         #Use default nsx-vlan-transportzone (from NSX-T 3.0...)
         $script:segment = Get-NSXTTransportZones -display_name nsx-vlan-transportzone | Add-NSXTPolicyInfraSegments -segment $pester_sg -vlan_ids 23
+        $script:tid = (Get-NSXTTransportZones -display_name nsx-vlan-transportzone).id
         $script:sid = $segment.unique_id
     }
 
@@ -48,10 +49,10 @@ Describe "Get Logical Switches" {
         $sg.display_name | Should -Be $pester_sg
     }
 
-    #It "Get Logical Switches by transport_zone(_id) ($pester_sg)" {
-    #    $sg = Get-NSXTLogicalSwitches -transport_zone_id $pester_sg
-    #    $sg.id | Should -Be $pester_sg
-    #}
+    It "Get Logical Switches by transport_zone(_id) ($tid)" {
+        $sg = Get-NSXTLogicalSwitches -transport_zone_id $tid
+        $sg.transport_zone_id | Should -Be $tid
+    }
 
     #It "Get Logical Switches by switching_profile_id ($pester_sg)" {
     #    $sg = Get-NSXTLogicalSwitches -switching_profile_id $pester_sg
